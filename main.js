2e5d5ede64740ef875fef1ecdd6a8579ad5302e1
PRASAD MADHURANGA <prasad.madhuranga@protonmail.com>
2025-12-19 22:10:47 +0530

    Refactor code structure for improved readability and maintainability and added Electron support.

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  const loadURL = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, 'out', 'index.html')}`;
+
+  win.loadURL(loadURL);
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
